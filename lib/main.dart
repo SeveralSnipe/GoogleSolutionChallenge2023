51f5cbc6184cf17792f8b24fddc8dfedd718d34c
SeveralSnipe <s.vishwakumar@gmail.com>
2023-03-23 13:22:37 +0530

    Changed nav

--- lib/main.dart
+++ lib/main.dart
@@ -44,40 +44,63 @@ class _MyHomePageState extends State<MyHomePage> {
         throw UnimplementedError("No widget for $selectedIndex");
     }
     return Scaffold(
-      body: Row(
-        children: [
-          SafeArea(
-            child: NavigationRail(
-              extended: false,
-              destinations: [
-                NavigationRailDestination(
-                  icon: Icon(Icons.medication),
-                  label: Text('Medication reminder'),
-                ),
-                NavigationRailDestination(
-                  icon: Icon(Icons.phone),
-                  label: Text('Contact Manager'),
-                ),
-                NavigationRailDestination(
-                  icon: Icon(Icons.menu_book),
-                  label: Text('Cognitive Testing'),
-                ),
-              ],
-              selectedIndex: selectedIndex,
-              onDestinationSelected: (value) {
-                setState(() {
-                  selectedIndex = value;
-                });
-              },
-            ),
-          ),
-          Expanded(
-            child: Container(
-              color: Theme.of(context).colorScheme.primary,
-              child: page,
-            ),
-          ),
+      // body: Row(
+      //   children: [
+      //     SafeArea(
+
+      // child: NavigationRail(
+      //   extended: false,
+      //   destinations: [
+      //     NavigationRailDestination(
+      //       icon: Icon(Icons.medication),
+      //       label: Text('Medication reminder'),
+      //     ),
+      //     NavigationRailDestination(
+      //       icon: Icon(Icons.phone),
+      //       label: Text('Contact Manager'),
+      //     ),
+      //     NavigationRailDestination(
+      //       icon: Icon(Icons.menu_book),
+      //       label: Text('Cognitive Testing'),
+      //     ),
+      //   ],
+      //   selectedIndex: selectedIndex,
+      //   onDestinationSelected: (value) {
+      //     setState(() {
+      //       selectedIndex = value;
+      //     });
+      //   },
+      // ),
+      //     ),
+      //     Expanded(
+      //       child: Container(
+      //         color: Theme.of(context).colorScheme.primary,
+      //         child: page,
+      //       ),
+      //     ),
+      //   ],
+      // ),
+      bottomNavigationBar: SafeArea(
+          child: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(Icons.medication), label: 'Medication'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.phone), label: 'Contact Manager'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.menu_book), label: 'Cognitive Testing'),
         ],
+        currentIndex: selectedIndex,
+        selectedItemColor: Colors.amber,
+        onTap: (value) {
+          setState(() {
+            selectedIndex = value;
+          });
+        },
+      )),
+      body: Container(
+        color: Theme.of(context).colorScheme.primary,
+        child: page,
       ),
     );
   }
